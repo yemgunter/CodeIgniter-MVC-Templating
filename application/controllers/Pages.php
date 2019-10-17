21ce57d2e42fb984b7923904c5427101fc27fcad
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-16 21:38:21 -0500

    Created Pages.php my super object
    Created a class named Pages, with a view method that accespts one argument smaed $page.  This Pages class has extended the CI_Controller class which means, the new pages class can access the methods and variables defined in the CI_Controller class. This is my first method.

---
+++ application/controllers/Pages.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Name: Yolanda Gunter
+ * Assignment: Coding 06
+ * Purpose: MVC Frameworks
+ * Notes: Learning Codeigniter to better understand MVC programming.
+ */
+
+class Pages extends CI_Controller {
+    
+    public function view($page = 'home ')
+    {
+    }
+}
