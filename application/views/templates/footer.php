273108df338c998614efbedc113e62a80e711fe0
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-16 22:27:41 -0500

    Created View template - header.php
    Although this is a .php file, it is the View Template for my page footer.  It contains basic (bottom portions) footer code that will dispaly before loading the main view together with a footer - it will also output the copyright symbol and 2019.

---
+++ application/views/templates/footer.php
@@ -0,0 +1,10 @@
+<!-- ****************************************************
+Name: Yolanda Gunter
+Assignment: Coding 06
+Purpose: MVC Frameworks
+Notes: Learning Codeigniter to better understand MVC programming.
+********************************************************* -->
+              
+                <em>&copy; 2019</em>
+        </body>
+</html>
