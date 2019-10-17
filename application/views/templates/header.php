2728577770676138b5fd6faea271715f83e06094
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-16 22:10:06 -0500

    Created View template - header.php
    Although this is a .php file, it is the View Template for my page headers.  It contains basic (top portions) header code that will dispaly before loading the main view together with a heading - it will also output the $title variable that will be defined in the controller later.

---
+++ application/views/templates/header.php
@@ -0,0 +1,14 @@
+<!-- ****************************************************
+Name: Yolanda Gunter
+Assignment: Coding 06
+Purpose: MVC Frameworks
+Notes: Learning Codeigniter to better understand MVC programming.
+********************************************************* -->
+
+<html>
+        <head>
+                <title>CodeIgniter Tutorial</title>
+        </head>
+        <body>
+
+                <h1><?php echo $title; ?></h1>
