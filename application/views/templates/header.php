f0c246b6c5fbcfa8b8e8588d27de0855c2b4603a
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-16 23:47:40 -0500

    Updated template header.php
    Added menu for test on server. will style later.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,11 +4,14 @@ Assignment: Coding 06
 Purpose: MVC Frameworks
 Notes: Learning Codeigniter to better understand MVC programming.
 ********************************************************* -->
-
+<!DOCTYPE html>
 <html>
         <head>
                 <title>CodeIgniter Tutorial</title>
         </head>
         <body>
-
+            <div class="nav-section">
+                <a href="views/pages/home.php" class="menu-link">Home</a>
+                <a href="views/pages/about.php" class="menu-link">About</a>
+            </div>
                 <h1><?php echo $title; ?></h1>
