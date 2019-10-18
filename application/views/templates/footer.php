b16756b2412faf3f5a7d0f26517abea885fb073e
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-17 23:35:05 -0500

    Updated footer.php
    Enhance footer for this application

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,7 +4,16 @@ Assignment: Coding 06
 Purpose: MVC Frameworks
 Notes: Learning Codeigniter to better understand MVC programming.
 ********************************************************* -->
-              
-                <em>&copy; 2019 Y. E. M. Gunter</em>
-        </body>
+<div class="row">
+    <div class="span12">
+        <div class="well">
+            <center> <em>Copyright &copy; 2019 Y. E. M. Gunter</em></center>
+        </div>
+    </div>
+</div>
+               
+                
+                
+       </div>
+    </body>
 </html>
