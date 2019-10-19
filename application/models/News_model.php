89cee702a1d43e830a0f2d22f6c1e7d3dd878753
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-19 00:24:10 -0500

    Ahead of myself...
    I got a lil ahead of my reading and actual assignment.

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -1,8 +1,15 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Name: Yolanda Gunter
+ * Assignment: Coding 06
+ * Purpose: MVC Frameworks
+ * Notes: Learning Codeigniter to better understand MVC programming.
  */
 
+class News_model extends CI_Model {
+    
+    public function __construct() {
+        $this->load->database();
+    }
+}
