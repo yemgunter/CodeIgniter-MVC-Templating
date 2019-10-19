75caf32cc5eeb8f7eb0735009087901d5a2ce078
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-19 13:07:43 -0500

    Update Pages.php controller
    Added nav template

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -20,7 +20,9 @@ class Pages extends CI_Controller {
         $data['title'] = ucfirst($page); // Capitalize the first letter
         
         $this->load->view('templates/header', $data);
+        $this->load->view('templates/nav', $data);
         $this->load->view('pages/'.$page, $data);
         $this->load->view('templates/footer', $data);
     }
+
 }
