38d4f55cc2c438459ba10e7872a37cc3631ea2d3
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-20 00:00:25 -0500

    Created nav.php

---
+++ application/views/templates/nav.php
@@ -0,0 +1,17 @@
+<!-- ****************************************************
+Name: Yolanda Gunter
+Assignment: Coding 06
+Purpose: MVC Frameworks
+Notes: Learning Codeigniter to better understand MVC programming.
+********************************************************* -->
+<div class="nav-section">
+    <ul>
+        <li><a href="index.php" class='home'>Home</a></li>
+        <li><a href="../pages/about.php">About</a></li>
+    </ul>
+</div>
+
+<div class="body">
+    <h1><?php echo $title; ?></h1>
+</div> 
+
