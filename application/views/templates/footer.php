b46d8b12153a955aca1477cecb07b1cf9bbdebc1
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-20 00:03:59 -0500

    Cleaned up footer. php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,16 +4,7 @@ Assignment: Coding 06
 Purpose: MVC Frameworks
 Notes: Learning Codeigniter to better understand MVC programming.
 ********************************************************* -->
-<div class="row">
-    <div class="span12">
-        <div class="well">
+
             <center> <em>Copyright &copy; 2019 Y. E. M. Gunter</em></center>
-        </div>
-    </div>
-</div>
-               
-                
-                
-       </div>
     </body>
 </html>
