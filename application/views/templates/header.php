5f2546e807553b69ed4888699b46aba18a4fda47
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-26 19:30:57 -0500

    Updated header but...
    I haven't uploaded this file to server yet everyithing i coded is showing up fine on a different device.
    
    I'm not sure if I  should bother changing it.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,14 +10,14 @@ Notes: Learning Codeigniter to better understand MVC programming.
             <title>Y Gunter - Coding 06 MVC</title>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="assets/favicon.ico" rel="icon" type="ico">
-            <link href="assets/main.css" rel="stylesheet" type="text/css">
+            <link href="assets/images/favicon.ico" rel="icon" type="ico">
+            <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/main.css">
             <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
             <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
         </head>
           <body>
               <div class="container">
                   <div class="headerlink">
-                      <img src="assets/banner.png" id="banner">
+                      <img src="<?php echo base_url(); ?>assets/images/banner.png" alt="banner">
                   </div>
               </div>
