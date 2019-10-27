b7916cd24a666bbe883c2c73a5f412dfc2436db5
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-27 12:38:35 -0500

    Update about.php

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -11,6 +11,9 @@ Notes: Learning Codeigniter to better understand MVC programming.
         <title>About</title>
     </head>
     <body>
-        My About Page!
+        <div class='bodywrapper'>
+            <h1>Hello! I am Yolanda Gunter - Full Stack Web Developer</h1>
+            <br />
+            <img src="assets/images/yemgunter.png" alt="Yolanda image"/>
+        </div>
     </body>
-</html>
