ac8a75f84a851b76126215da6a8cecc7f6ead74e
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-27 12:46:19 -0500

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,14 +10,12 @@ Notes: Learning Codeigniter to better understand MVC programming.
             <title>Y Gunter - Coding 06 MVC</title>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="assets/images/favicon.ico" rel="icon" type="ico">
-            <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/main.css">
+            <link href="assets/images/favicon.ico" rel="icon" type="image/ico">
+            <link href="assets/css/main.css" rel="stylesheet" type="text/css">
             <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
             <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
         </head>
           <body>
-              <div class="container">
-                  <div class="headerlink">
-                      <img src="<?php echo base_url(); ?>assets/images/banner.png" alt="banner">
-                  </div>
+              <div class="header_banner"> 
+                  <img src="assets/images/banner.png" alt="banner" id="banner">
               </div>
