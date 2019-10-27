108e2e88d8a2e44e353003d613bb8f2bc18ed230
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-27 12:48:29 -0500

    Update nav.php

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -5,13 +5,10 @@ Purpose: MVC Frameworks
 Notes: Learning Codeigniter to better understand MVC programming.
 ********************************************************* -->
 <div class="nav-section">
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about">About</a></li>
-    </ul>
+    <a title="Home" accesskey="A" href="welcome">Home</a>
+    <a title="About" accesskey="B" href="welcome/about">About</a>
 </div>
 
 <div class="body">
     <h1><?php echo $title; ?></h1>
 </div> 
-
