b31168de242310d83914b9ebe35dae1a1d74c772
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-27 21:17:47 -0500

    Updated about.php

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -13,7 +13,19 @@ Notes: Learning Codeigniter to better understand MVC programming.
     <body>
         <div class='bodywrapper'>
             <h1>Hello! I am Yolanda Gunter - Full Stack Web Developer</h1>
-            <br />
-            <img src="assets/images/yemgunter.png" alt="Yolanda image"/>
+             <div id="container">
+                <div id="body">
+                    <h2>My handle across all social networks is "yemgunter".</h2>
+                    <p><img src="assets/images/yemgunter.png" alt="Yolanda image"/></p>
+                    <p>I'm a super creative. Meaning I create across multiple mediums.</p>
+                    <ul>
+                    <li>Painting
+                    <li>Sewing
+                    <li>Jewelry Making
+                    </ul>
+                    
+                    <p>Follow me online. Check the footer to connect.</p>
+                </div>
+            </div>
         </div>
     </body>
