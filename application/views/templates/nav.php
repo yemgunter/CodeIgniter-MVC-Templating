bca253fe5f55c348c66b07b52018174b152a8fe8
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-27 21:49:08 -0500

    Update nav.php - added target="new" for About ...
    ...unable to fix my navigation.  After hundreds of articles, videos, tutorials and hours reading User Guide - my navigation does not work.  Tried every conceivable way to code it to reach my About.php - failed.

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -5,10 +5,11 @@ Purpose: MVC Frameworks
 Notes: Learning Codeigniter to better understand MVC programming.
 ********************************************************* -->
 <div class="nav-section">
-    <a title="Home" accesskey="A" href="welcome">Home</a>
-    <a title="About" accesskey="B" href="index.php/about">About</a>
+    <a title="Home" accesskey="A" href="index.php">Home</a>
+    <a title="About" accesskey="B" href="../pages/" target='new'>About</a>
 </div>
 
 <div class="body">
     <h1><?php echo $title; ?></h1>
+    <br/>
 </div> 
