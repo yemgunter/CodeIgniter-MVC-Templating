789e1c12b38f5e923f94b56163170e59449f996f
Yolanda Gunter <yolanda.mcrae@g.austincc.edu>
2019-10-27 19:23:40 -0500

    Update nav.php
    Fooled around with that navigation again... sigh.  I just do not get it. :tired_face:

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -6,7 +6,7 @@ Notes: Learning Codeigniter to better understand MVC programming.
 ********************************************************* -->
 <div class="nav-section">
     <a title="Home" accesskey="A" href="welcome">Home</a>
-    <a title="About" accesskey="B" href="welcome/about">About</a>
+    <a title="About" accesskey="B" href="index.php/about">About</a>
 </div>
 
 <div class="body">
